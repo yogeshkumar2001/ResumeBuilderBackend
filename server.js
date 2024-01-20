6fb58f1ae346634d38fcd60ab2ada3caf69bd716
Yogesh Kumar <yk9599493681@gmail.com>
2024-01-20 12:59:21 +0530

    connect mongodb

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express()
 const route = require("./Route/index.js")
 const dotenv = require("dotenv");
 const cors = require("cors");
+const {connectDB} = require("./config/index.js");
 dotenv.config();
 
 app.use(cors()); //CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 
 app.use(express.json()); //it is used to parse incoming JSON data in the request body. This middleware is necessary when dealing with JSON data in POST requests,
-
+// connectDB()
 app.use("/api", route)
 app.listen(process.env.PORT || 9000, function () {
     console.log(process.env.PORT || 9000)
 })
+
+// mongoose.connect(process.env.DB_URL);
+connectDB();
